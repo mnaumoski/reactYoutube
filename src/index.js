962bf88521d7d54672d8bb0559b1002a1e51208b
mnaumoski <naumoski.marija@gmail.com>
2017-02-09 12:58:38 -0500

    selected video loads

--- src/index.js
+++ src/index.js
@@ -12,10 +12,20 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({ videos });
+     this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+        this.setState({ 
+          videos: videos,
+          selectedVideo: videos[0] 
+        });
         // this.setState({ videos: videos });
       });
   }
@@ -23,9 +33,11 @@ class App extends Component {
   render() {
   return (
     <div>
-      <SearchBar />
-      <VideoDetail video={this.state.videos[0]}/>
-      <VideoList videos={this.state.videos} />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList 
+      onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+      videos={this.state.videos} />
 
     </div>
     );
