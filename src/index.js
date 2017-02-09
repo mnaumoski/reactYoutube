d80287a6d3bba0ffa76646286ba01ef7da0d94b2
mnaumoski <naumoski.marija@gmail.com>
2017-02-09 13:56:48 -0500

    used debounce to delay search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -6,13 +7,11 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBO8AkKl_DkFXCwAg9ABAzCvoCSVy84Dec';
 
-
-
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
+    this.state = {
       videos: [],
       selectedVideo: null
      };
@@ -22,25 +21,27 @@ class App extends Component {
 
   videoSearch(term) {
       YTSearch({key: API_KEY, term: term}, (videos) => {
-        this.setState({ 
+        this.setState({
           videos: videos,
-          selectedVideo: videos[0] 
+          selectedVideo: videos[0]
         });
         // this.setState({ videos: videos });
       });
   }
 
   render() {
-  return (
-    <div>
-      <SearchBar onSearchTermChange={term => this.videoSearch(term)/>
-      <VideoDetail video={this.state.selectedVideo}/>
-      <VideoList 
-      onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
-      videos={this.state.videos} />
-
-    </div>
-    );
+  
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300)
+    
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
+      </div>
+      );
 }
 }
 
